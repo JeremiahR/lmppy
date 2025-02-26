919390db561511fbef41b63706a31823a147d3ff
jeremiah <me@jeremiahrogers.com>
2025-02-25 21:24:38 -0500

    accepts pings

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import sys
 import socket
-from pyln.proto.wire import LightningConnection
+from pyln.proto.wire import connect
 from pyln.proto.primitives import PrivateKey, PublicKey
 from ecdsa import SigningKey, SECP256k1
+from binascii import hexlify
+
+MESSAGE_TYPES = {
+    16: "Init",
+}
+
+
+def diagnose(message):
+    print("Diagnosing message...")
+    message_type = int.from_bytes(message[:2], byteorder="big", signed=True)
+    print("Message type: ", MESSAGE_TYPES.get(message_type, message_type))
+    print("Message length: ", len(message))
+    print("Message content: ", hexlify(message[2:]).decode("ASCII"))
 
 
 def main():
@@ -11,12 +24,12 @@ def main():
         sys.exit(1)
     peer = sys.argv[1]
 
-    remote_pubkey, host = peer.split("@")
+    node_id, host = peer.split("@")
     host, port = host.split(":")
     port = int(port)
-    print(f"Host: {host}, Port: {port}, Public Key: {remote_pubkey}")
+    print(f"Host: {host}, Port: {port}, Public Key: {node_id}")
 
-    remote_pubkey = PublicKey(bytes.fromhex(remote_pubkey))
+    node_id = PublicKey(bytes.fromhex(node_id))
 
     private_key = SigningKey.generate(curve=SECP256k1)
     private_key = PrivateKey(private_key.to_string())
@@ -24,8 +37,15 @@ def main():
     sock = socket.socket()
     sock.connect((host, port))
 
-    lc = LightningConnection(sock, remote_pubkey, private_key, True)
-    lc.init_handshake()
+    lc = connect(private_key, node_id, host, port=port, socks_addr=None)
+    # Send an init message, with no global features, and 0b10101010 as local
+    # features.
+    lc.send_message(b"\x00\x10\x00\x00\x00\x01\xaa")
+
+    # Now just read whatever our peer decides to send us
+    while True:
+        message = lc.read_message()
+        diagnose(message)
 
 
 if __name__ == "__main__":
